1550f6beec5b492af12d3802b9c1e15b7e1c6137
homers8 <ffeier@gmx.de>
2025-07-27 19:29:54 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 from threading import Thread
 
 app = Flask(__name__)
+BOT = None  # Wird später vom Hauptprogramm gesetzt
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return "Bot läuft! ✅"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if BOT:
+        update = BOT.update_queue.put(request.get_json(force=True))
+    return '', 200
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
-def keep_alive():
+def keep_alive(application):
+    global BOT
+    BOT = application
     t = Thread(target=run)
     t.start()
